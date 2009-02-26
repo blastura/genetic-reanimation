e596b8ad565db012d6c664350f23ed4f563a2ee9
Anton Johansson <anton.johansson@gmail.com>
2009-02-26 13:53:29 +0100

    libs

---
+++ prj.el
@@ -0,0 +1,12 @@
+;; (my-get-classpath)
+(set-variable 'jde-global-classpath
+              (list "/Users/anton/Documents/skola/emergenta-system/projektet/bin/main"
+                    "/Users/anton/Documents/skola/emergenta-system/projektet/bin/test"
+                    "/Users/anton/Documents/skola/emergenta-system/projektet/lib/junit-4.5.jar"
+                    "/Users/anton/Documents/skola/emergenta-system/projektet/lib/phys2d-060408.jar"))
+
+
+;;(setq jdibug-connect-host "localhost"
+;;      jdibug-connect-port 5005)
+
+(message "Loaded prj.el")
