5fbda507355695be992ca59d529cd33bd9e1c2f3
Anton Johansson <anton.johansson@gmail.com>
2009-03-10 14:47:09 +0100

    Creature connectToWorld

--- src/se/umu/cs/geneticReanimation/Creature.java
+++ src/se/umu/cs/geneticReanimation/Creature.java
@@ -1,7 +1,10 @@
 package se.umu.cs.geneticReanimation;
 
+import net.phys2d.raw.World;
+
 // Extend Clonable?
 public interface Creature {
+    public void connectToWorld(World world);
     public void act();
     public double getFitness();
     public void setFitness(double fitness);
--- src/se/umu/cs/geneticReanimation/WormCreature.java
+++ src/se/umu/cs/geneticReanimation/WormCreature.java
@@ -1,14 +1,14 @@
 package se.umu.cs.geneticReanimation;
 
+import java.util.ArrayList;
+import java.util.List;
 import net.phys2d.math.Vector2f;
-import net.phys2d.raw.AngleJoint;
-import net.phys2d.raw.BasicJoint;
 import net.phys2d.raw.Body;
-import net.phys2d.raw.FixedJoint;
+import net.phys2d.raw.DistanceJoint;
+import net.phys2d.raw.Joint;
+import net.phys2d.raw.SpringyAngleJoint;
 import net.phys2d.raw.World;
 import net.phys2d.raw.shapes.Box;
-import net.phys2d.raw.shapes.Circle;
-
 import se.umu.cs.geneticReanimation.neuralnet.Brain;
 import se.umu.cs.geneticReanimation.neuralnet.HopfieldNeuralNet;
 
@@ -16,40 +16,17 @@ public class WormCreature implements Creature {
     private double fitness;
     private Brain brain;
     private World world;
-
-    /* Constants */
-    private float r = 20f;
-    private float bodyLength = 45f;
-    private float legWidth = 5f;
-    private float legHeight = 20f;
-    private float spaceing = 10f;
-    private float mass_hip = 10;
-    private float mass_thigh = 2f;
-    private float mass_leg = 2f;
-
-    /* Body parts */
-    // Hip
-    private Body hipLeft;
-    private Body hipRight;
-    // Thigh
-    private Body thighLeft;
-    private Body thighRight;
-    // Leg
-    private Body legLeft;
-    private Body legRight;
-    // Paw
-    private Body pawLeft;
-    private Body pawRight;
-
-
-    public WormCreature(World world, double[] genotype) {
-        this.world = world;
+    private List<Body> bodyList;
+    private List<Joint> jointList;
+    
+    public WormCreature(double[] genotype) {
         this.brain = new HopfieldNeuralNet(genotype);
+        this.bodyList = new ArrayList<Body>();
+        this.jointList = new ArrayList<Joint>();
+        initBody();
     }
-
-    public WormCreature(World world) {
-        this.world = world;
-
+    
+    public WormCreature() {
         // TODO: decide from input output
         int genotypeSize = 5 * 5;
         double[] newGenotype = new double[genotypeSize];
@@ -57,26 +34,69 @@ public class WormCreature implements Creature {
             newGenotype[i] = ((Math.random()*2)-1);
         }
         this.brain = new HopfieldNeuralNet(newGenotype);
-        initDog();
+        this.bodyList = new ArrayList<Body>();
+        this.jointList = new ArrayList<Joint>();
+        initBody();
+    }
+    
+    public void connectToWorld(World world) {
+        for (Body b : bodyList) {
+            world.add(b);
+        }
+        for (Joint j : jointList) {
+            world.add(j);
+        }        
+    }
+
+    private void initBody() {
+        // Make schneyk
+        float sWidth = 20f;
+        float sHeight = 10f;
+        float spaceing = 10f;
+        int sections = 5;
+
+        SpringyAngleJoint[] saj;
+        saj = new SpringyAngleJoint[sections-1];
+
+        Body segment = null;
+        Body prev_segment;
+
+        for(int i=0; i<sections; i++) {
+            prev_segment = segment;
+            segment = new Body("Segment", new Box(sWidth, sHeight), 1);
+            segment.setPosition((sWidth + spaceing)*i, 0);
+            bodyList.add(segment);
+            if(i > 0) {
+                Vector2f fixpoint1 = new Vector2f(sWidth/2f, 0);
+                Vector2f fixpoint2 = new Vector2f(-sWidth/2f, 0);
+                DistanceJoint dj = new DistanceJoint(prev_segment, segment,
+                                                     fixpoint1, fixpoint2, spaceing);
+                jointList.add(dj);
+
+                saj[i-1] = new SpringyAngleJoint(prev_segment, segment,
+                                                 fixpoint1, fixpoint2, 1000f, 0f);
+                world.add(saj[i-1]);
+            }
+        }
     }
     
     public void act() {
-        double[] inputs = {hipRight.getPosition().getY(),
-                           hipLeft.getPosition().getY(),
-                           legLeft.getPosition().getX(),                           
-                           legRight.getPosition().getY()};
-        brain.setInputs(inputs);
-        brain.step();
-        brain.step();
-        double[] output = brain.getOutputs();
+        //         double[] inputs = {hipRight.getPosition().getY(),
+        //                            hipLeft.getPosition().getY(),
+        //                            legLeft.getPosition().getX(),                           
+        //                            legRight.getPosition().getY()};
+        //         brain.setInputs(inputs);
+        //         brain.step();
+        //         brain.step();
+        //         double[] output = brain.getOutputs();
         
-        // Affect worm
-        legLeft.adjustAngularVelocity((float) output[2] * 2);
-        legRight.adjustAngularVelocity((float) output[2] * 2);
+        //         // Affect worm
+        //         legLeft.adjustAngularVelocity((float) output[2] * 2);
+        //         legRight.adjustAngularVelocity((float) output[2] * 2);
     }
     
     public double getFitness() {
-        this.fitness = getMidPosition(hipRight, hipLeft).getX();
+        //this.fitness = getMidPosition(hipRight, hipLeft).getX();
         return this.fitness;
     }
 
@@ -86,62 +106,6 @@ public class WormCreature implements Creature {
 
     public double[] getGenotype() {
         return brain.getGenotype();
-    }
-
-    private void initDog() {
-        /* Make bodyparts */
-        hipLeft = new Body("", new Circle(r), 10);
-        hipLeft.setPosition(0, 0);
-        world.add(hipLeft);
-
-        this.thighLeft = new Body("thighLeft", new Box(legWidth, legHeight), 10);
-        thighLeft.setPosition(0, r + legHeight / 2 + spaceing);
-        world.add(thighLeft);
-
-        legLeft = new Body("legLeft", new Box(legWidth, legHeight), 10);
-        legLeft.setPosition(0, r + (legHeight / 2) + legHeight + spaceing * 2);
-        world.add(legLeft);
-
-        hipRight = new Body("", new Circle(r), 10);
-        hipRight.setPosition(bodyLength, 0);
-        world.add(hipRight);
-
-        thighRight = new Body("thighRight", new Box(legWidth, legHeight), 10);
-        thighRight.setPosition(bodyLength, r + legHeight / 2 + spaceing);
-        world.add(thighRight);
-
-        legRight = new Body("thighRight2", new Box(legWidth, legHeight), 10);
-        legRight.setPosition(bodyLength, r + (legHeight / 2) + legHeight + spaceing * 2);
-        world.add(legRight);
-
-        /* Joints */
-        BasicJoint hipLeftJoint = new BasicJoint(hipLeft, thighLeft,
-                                                 (Vector2f) hipLeft.getPosition());
-        world.add(hipLeftJoint);
-
-        BasicJoint leftKneeJoint = new BasicJoint(thighLeft, legLeft,
-                                                  getMidPosition(thighLeft, legLeft));
-        world.add(leftKneeJoint);
-
-        AngleJoint leftKneeAngleJoint = new AngleJoint(thighLeft, legLeft,
-                                                       new Vector2f(),
-                                                       new Vector2f(),
-                                                       (float) Math.PI / 2,
-                                                       0f);
-        world.add(leftKneeAngleJoint);
-
-        BasicJoint hipRightJoint = new BasicJoint(hipRight, thighRight,
-                                                  (Vector2f) hipRight.getPosition());
-        world.add(hipRightJoint);
-
-        BasicJoint rightKneeJoint = new BasicJoint(thighRight, legRight,
-                                                   getMidPosition(thighRight, legRight));
-
-        world.add(rightKneeJoint);
-
-        // Connect hips
-        FixedJoint backbone = new FixedJoint(hipLeft, hipRight);
-        world.add(backbone);
     }
 
     private Vector2f getMidPosition(Body b1, Body b2) {
