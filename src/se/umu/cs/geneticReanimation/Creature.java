c80effd14fc0996986c3300ae95a49f1f383833d
Anton Johansson <anton.johansson@gmail.com>
2009-03-10 13:57:35 +0100

    act()

--- src/se/umu/cs/geneticReanimation/Creature.java
+++ src/se/umu/cs/geneticReanimation/Creature.java
@@ -2,9 +2,8 @@ package se.umu.cs.geneticReanimation;
 
 // Extend Clonable?
 public interface Creature {
+    public void act();
     public double getFitness();
-
     public void setFitness(double fitness);
-
     public double[] getGenotype();
 }
