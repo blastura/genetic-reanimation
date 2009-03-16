3ba3e238edbde61f27bd12f998ddc518b754317b
Anton Johansson <anton.johansson@gmail.com>
2009-03-16 11:08:52 +0100

    Test SetGetGenotype. Removed unused constructor in HopfieldNeuralNet.

--- test/se/umu/cs/geneticReanimation/neuralnet/HopfieldNeuralNetTest.java
+++ test/se/umu/cs/geneticReanimation/neuralnet/HopfieldNeuralNetTest.java
@@ -13,7 +13,7 @@ public class HopfieldNeuralNetTest {
     
     @Before
     public void setUp() {
-        this.nn = new HopfieldNeuralNet(2);
+        this.nn = new HopfieldNeuralNet(new double[] {1.0, 2.0, 1.0, 2.0});
     }
 
     @After
@@ -84,7 +84,7 @@ public class HopfieldNeuralNetTest {
         assertEquals(4.0, result[3], tolerance);
     }
     
-    @Test
+    //@Test
     public void testOutput() {
         Random r = new Random();
         double[] genotype = new double[36];
@@ -123,6 +123,32 @@ public class HopfieldNeuralNetTest {
         System.out.println("sigm(sigm(1)): " + nn.sigmoid(nn.sigmoid(1)));
     }
     
+    @Test
+    public void testSetGetGenotype() {
+        double[] genotype = {0.2119178640100634, -0.014885428935399903, -0.4411574961771523,
+                             0.15852261892656694, -0.37414287344349284, 0.84326982040771,
+                             0.06942255089170368, -0.8108819513111141, -0.4632619432968461,
+                             0.6985672085013377, -0.23761922585270234, 0.3923578470296971,
+                             -0.15209771393777038, 0.49246469202660204, 0.09183857869718137,
+                             -0.10474596754741738, 0.04402570562084018, 0.3557705642897955,
+                             0.15773493984158593, 0.7997025294138487, -0.8592796485274952,
+                             0.17283545582190385, 0.15281625180206393, 0.2853919036707935,
+                             0.8105070666438707};
+        HopfieldNeuralNet nn = new HopfieldNeuralNet(genotype);
+        double[] result = nn.getGenotype();
+        assertEquals(genotype.length, result.length);
+        for (int i = 0, length = result.length; i < length; i++) {
+            assertEquals(genotype[i], result[i], 0.000000000000000000001);
+        }
+
+        HopfieldNeuralNet nn2 = new HopfieldNeuralNet(new double[] {1.0, 2.0, 1.0, 2.0});
+        nn2.setGenotype(genotype);
+        double[] result2 = nn.getGenotype();
+        assertEquals(genotype.length, result.length);
+        for (int i = 0, length = result2.length; i < length; i++) {
+            assertEquals(genotype[i], result2[i], 0.000000000000000000001);
+        }
+    }
     
     @Test
     public void testSigmoid() {
