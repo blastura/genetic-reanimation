89b0e5e23a23e0d39bddad56fc478ff9d899280c
Ludvig Widman <dit06lwn@salt.cs.umu.se>
2009-03-17 16:01:01 +0100

    reportstuff

--- src/se/umu/cs/geneticReanimation/GeneticAlgoritm.java
+++ src/se/umu/cs/geneticReanimation/GeneticAlgoritm.java
@@ -65,7 +65,7 @@ public class GeneticAlgoritm {
             newPopulation.add(newParent);
         }
 
-        for (int i = 0; i < populationSize; i++) {
+        for (int i = 1; i < populationSize; i++) {
             mutate(newPopulation.get(i));
         }
 
